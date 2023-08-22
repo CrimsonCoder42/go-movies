4de546381c6df42fda70ac358095c8a8a3bf767a
CrimsonCoder42 <nostro37@gmail.com>
2023-08-22 11:04:10 -0400

    middleware complete and CORS updated

--- go-movies-back-end/cmd/api/handlers.go
+++ go-movies-back-end/cmd/api/handlers.go
@@ -1,10 +1,74 @@
 package main
 
 import (
+	"backend/internal/models"
+	"encoding/json"
 	"fmt"
 	"net/http"
+	"time"
 )
 
 func (app *application) Home(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello, world from %s", app.Domain)
+	var payload = struct {
+		Version string `json:"version"`
+		Status  string `json:"status"`
+		Message string `json:"message"`
+	}{
+		Status:  "active",
+		Message: "Go Movies up and running",
+		Version: "1.0.0",
+	}
+
+	out, err := json.Marshal(payload)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(out)
+}
+
+func (app *application) AllMovies(w http.ResponseWriter, r *http.Request) {
+	var movies []models.Movie
+
+	rd, _ := time.Parse("2006-01-02", "1986-03-07")
+
+	highlander := models.Movie{
+		ID:          1,
+		Title:       "Highlander",
+		ReleaseDate: rd,
+		Runtime:     116,
+		MPAARating:  "R",
+		Description: "There can be only one",
+		CreatedAt:  time.Now(),
+		UpdatedAt:  time.Now(),
+	}
+
+	movies = append(movies, highlander)
+
+	rd, _ = time.Parse("2006-01-02", "1981-06-12")
+
+	rotla := models.Movie{
+		ID:          2,
+		Title:       "Raiders of the Lost Ark",
+		ReleaseDate: rd,
+		Runtime:     115,
+		MPAARating:  "PG-13",
+		Description: "Indiana Jones and the Raiders of the Lost Ark",
+		CreatedAt:  time.Now(),
+		UpdatedAt:  time.Now(),
+	}
+
+	movies = append(movies, rotla)
+
+	out, err := json.Marshal(movies)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(out)
+
 }
